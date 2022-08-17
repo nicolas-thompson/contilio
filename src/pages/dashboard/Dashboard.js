f0a13fd9ddbaa43f7dbaaa0ef98a81c832da816c
Nicolas Thompson <mail@nicolasthompson.com>
2022-08-17 13:11:32 +0100

    Fetches data from data file

--- src/pages/dashboard/Dashboard.js
+++ src/pages/dashboard/Dashboard.js
@@ -3,6 +3,31 @@ import React from 'react';
 import Layout from '../../components/layout/Layout';
 
 class Dashboard extends React.Component {
+
+
+  getData = () => {
+    fetch('data.json'
+      , {
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        }
+      }
+    )
+      .then(function (response) {
+        console.log(response)
+        return response.json();
+      })
+      .then(function (myJson) {
+        console.log(myJson);
+      });
+  }
+
+  componentDidMount() {
+    console.log('Dashboard mounted');
+    this.getData();
+  }
+
   render() {
     return (
       <Layout>
